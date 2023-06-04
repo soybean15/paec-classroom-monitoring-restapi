4b1aad58fc7251c4400293ab2f3a9f0a49e3e4ad
soybean15 <marlonpadilla1593@gmail.com>
2023-06-04 21:06:18 +0800

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\RoutePath;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
