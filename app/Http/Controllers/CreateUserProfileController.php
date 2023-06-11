8f67da1efa406255c515eaf1664eaf2d26a7eab3
soybean15 <marlonpadilla1593@gmail.com>
2023-06-11 21:40:42 +0800

    added create user profile

--- app/Http/Controllers/CreateUserProfileController.php
+++ app/Http/Controllers/CreateUserProfileController.php
@@ -10,8 +10,28 @@ class CreateUserProfileController extends Controller
     public function store(Request $request){
 
 
+        $user = $request->user();
+
+        $userProfile = $user->profile;
+        if (!$userProfile) {
+            $userProfile = new \App\Models\UserProfile();
+            $userProfile->user_id = $user->id;
+        }
+    
+        $userProfile->firstname = $request->input('firstname');
+        $userProfile->lastname = $request->input('lastname');
+        $userProfile->middlename = $request->input('middlename');
+        $userProfile->gender = $request->input('gender');
+        $userProfile->birthdate = $request->input('birthdate');
+        $userProfile->contact_number = $request->input('contact_number');
+        $userProfile->image = $request->input('image');
+        $userProfile->address = $request->input('address');
+    
+        $userProfile->save();
+    
         return response()->json([
-            'request' => $request->all()
+            'message' => 'User profile updated successfully.',
+            
         ]);
 
     }
