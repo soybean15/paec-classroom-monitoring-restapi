d4d00f8362128461606ab510b107e755d607f752
soybean15 <marlonpadilla1593@gmail.com>
2023-06-17 23:00:21 +0800

    added pendin users

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,21 +7,42 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     //
-    public function index(){
+    public function index()
+    {
         $users = \DB::table('users')
             ->join('pending_request', 'users.id', '=', 'pending_request.user_id')
             ->select('users.*')
             ->distinct()
             ->get();
 
-            return response()->json([
-                'users' => $users,
-                             
-            ]);
+        return response()->json([
+            'pendingUsers' => $users,
+
+        ]);
     }
 
-    public static function getUsersWithPendingRequests()
+    public static function getUsers()
     {
-        
+        $pendingUsers = \DB::table('users')
+            ->whereIn('id', function ($query) {
+                $query->select('user_id')
+                    ->from('pending_request');
+            })
+            ->distinct()
+            ->get();
+
+        $users = \DB::table('users')
+            ->whereNotIn('id', function ($query) {
+                $query->select('user_id')
+                    ->from('pending_request');
+            })
+            ->distinct()
+            ->get();
+
+        return response()->json([
+            'pendingUsers' => $pendingUsers,
+            'users' => $users
+
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -47,6 +47,7 @@ Route::prefix('user')->middleware('auth:sanctum')->group(function () {
 Route::prefix('admin')->middleware(['auth:sanctum','isAdmin'])->group(function () {
    
     Route::get('/', [\App\Http\Controllers\AdminController::class, 'index']);
+    Route::get('/users', [\App\Http\Controllers\AdminController::class, 'getUsers']);
 });
 
 Route::get('roles',[\App\Http\Controllers\RoleController::class, 'index']);
