0772af2f8f4bb054fe0dea4ad2ac1ea060dcc863
soybean15 <marlonpadilla1593@gmail.com>
2023-06-30 19:23:23 +0800

    added schedule conflict

--- app/Http/Controllers/Admin/ScheduleController.php
+++ app/Http/Controllers/Admin/ScheduleController.php
@@ -4,36 +4,91 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\JsonResponse;
 
 class ScheduleController extends Controller
 {
     //
 
-    public function addSchedule(Request $request){
+
+
+    public function addSchedule(Request $request)
+    {
+
+
+
+        $start = $request['start'];
+        $end = $request['end'];
+        $day = $request['day'];
+
+        $conflictingSchedules = \DB::table('schedules')
+            ->where(function ($query) use ($start, $end,$day) {
+
+                $query->where('end', '>', $start)
+                    ->where('start', '<', $end)
+                       ->where('day', $day);
+
+            })->where('subject_teacher_id', $request['subject_teacher_id'])
+            ->get();
+
+        if (!$conflictingSchedules->isEmpty()) {
+            return new JsonResponse([
+                'errors' => [
+                    'start'=>['Schedule Conflict']
+                ]
+            ], 422);
+        }
+
+
+
+
+
+
+
+        $validator = Validator::make($request->all(), [
+            'start' => 'required|date_format:H:i',
+            'end' => 'required|date_format:H:i',
+            'day' => 'required',
+            // Add more validation rules if needed
+        ]);
+
+
+
+
+
+
+        if ($validator->fails()) {
+            // Return JSON response with validation errors
+            return new JsonResponse([
+                'errors' => $validator->errors()
+            ], 422);
+        }
 
 
         $schedule = \App\Models\Schedule::create([
-            'start'=>$request['start'],
-            'end'=> $request['end'],
-            'day'=>$request['day'],
-            'room_id'=>$request['room'],
-            'section'=> $request['prefix'] . $request['section'],
-            'subject_teacher_id'=>$request['subject_teacher_id']
+            'start' => $request['start'],
+            'end' => $request['end'],
+            'day' => $request['day'],
+            'room_id' => $request['room'],
+            'section' => $request['prefix'] . $request['section'],
+            'subject_teacher_id' => $request['subject_teacher_id']
 
         ]);
         return response()->json([
-            'request'=>$schedule
+            'request' => $schedule
         ]);
 
     }
 
-    public function getSchedule($id){
-        $schedules = \App\Models\Schedule::where('subject_teacher_id',$id)->get();
+    public function getSchedule($id)
+    {
+        $schedules = \App\Models\Schedule::where('subject_teacher_id', $id)->get();
 
 
         return response()->json([
-            'schedules'=>$schedules,
-         
+            'schedules' => $schedules,
+
         ]);
     }
 }
--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -10,13 +10,35 @@ class Schedule extends Model
     use HasFactory;
 
     protected $fillable = [
-        'start', 
-        'end', 
-        'day', 
-        'room_id', 
+        'start',
+        'end',
+        'day',
+        'room_id',
         'section',
-       'subject_teacher_id'
+        'subject_teacher_id'
     ];
+
+    public function getDayAttribute($value)
+    {
+       
+        switch ($value) {
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+                return "Saturday";
+            default:
+                return $value;
+
+        }
+    }
 
 
 
